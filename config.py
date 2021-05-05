0302fa3a2a45c380f633bf8746f005e52effc0c0
VladKozlovskiy <kozlovskij.vu@phystech.edu>
2021-05-06 00:59:27 +0300

    added Readme`

---
+++ config.py
@@ -0,0 +1,10 @@
+
+token = '1756290871:AAGj8_apOkmPvisYkES8_mhhYXr-vn3jnFA'
+Beginning = 'https://www.wildberries.ru/catalog/obuv/muzhskaya?sort='
+HOST = 'https://www.wildberries.ru/'
+page = '&page='
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0'}
+class_of_title = 'goods-name c-text-sm'
+class_of_item = 'dtList-inner'
+class_of_brand = 'brand-name c-text-sm'
+class_of_link = 'ref_goods_n_p j-open-full-product-card'
